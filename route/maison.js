61e75c7af109e2758fa9afa3370369f85fa25281
maaxcode <74791725+maaxcode@users.noreply.github.com>
2023-02-01 16:47:05 +0100

    simulation température et geteur et setteur

--- route/maison.js
+++ route/maison.js
@@ -1,10 +1,24 @@
 
-EntréeTemp = 10
-SortieTemp = 25
+EntréeTemp = 0
+SortieTemp = 0
 EntréeLum = false 
 SortieLum = true 
+
+if(EntréeLum!=SortieLum)
+{
+    if(EntréeLum== true)
+    {
+        LumiereOn = true;
+    }else{
+        LumiereOn = false;
+    }
+}
+
+
 EntréeVolet = false 
 SortieVolet = true 
+
+
 
 
 const express = require('express');
@@ -14,7 +28,30 @@ routeur.use('/test',(req, res) => {
  });
 
  routeur.get('/getsortietemp',(req, res) => {
-    res.status(200).json({SortieTemp});
+    res.status(200).json({SortieTemp,});
+ });
+
+ routeur.get('/setentreetemp:value',(req, res) => {
+
+    if (req.params.value <= 25 && req.params.value >= 15)
+    {
+        console.log("Dans le set temp");
+        EntréeTemp = req.params.value;
+        while (EntréeTemp!=SortieTemp)
+        {
+            if (EntréeTemp>SortieTemp)
+            {
+                SortieTemp=SortieTemp+1;
+                console.log(SortieTemp);
+            }else{
+                SortieTemp=SortieTemp-1;
+                console.log(SortieTemp);
+            }
+        }
+        res.status(200).json({EntréeTemp});
+    }else 
+    res.status(201).json({message : "Valeur incorrecte, entrez une valeur comprise entre 15 et 25 "});
+    
  });
 
  routeur.get('/getsortielum',(req, res) => {
@@ -35,6 +72,7 @@ routeur.use('/test',(req, res) => {
  routeur.get('/getentreevolet',(req, res) => {
     res.status(200).json({EntréeVolet});
  });
+
 
 
 
