f06dff3d0005a4bd003c0d6a2a6e4b32a99b5491
ThomasMaille <thomas.maille13@gmail.com>
2023-02-15 16:40:48 +0100

    "Creation commande Ping (test)"

--- BotMaison/main.js
+++ BotMaison/main.js
@@ -1,8 +1,17 @@
 const Discord = require("discord.js")
-const bot = new Discord.Client({intents: 3276799})
+const intents = new Discord.IntentsBitField(3276799)
+const bot = new Discord.Client({intents})
+const loadCommands = require("./Loaders/loadCommands")
 const config = require("./config")
 
+bot.commands = new Discord.Collection()
+
 bot.login(config.token)
+loadCommands(bot)
+
+bot.on("messageCreate", async message => {
+    if(message.content === "!ping") return bot.commands.get("ping").run(bot, message)
+})
 
 bot.on("ready", async () => {
 
