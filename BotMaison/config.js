c812ede93787a6b0208586ac7ae102f04f7fb86e
ThomasMaille <thomas.maille13@gmail.com>
2023-02-15 14:29:25 +0100

    ajout bot maison

---
+++ BotMaison/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    token: "MTA3MjkwMTA0NjMxMTA3NTg4Mg.GMKv2S.LfY-j4h6WhalHNQ0UhsCzsUAJ3kIjRPhOkCHhE"
+}
---
+++ BotMaison/main.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client({intents: 3276799})
+const config = require("./config")
+
+bot.login(config.token)
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} est en ligne!`)
+
+})
