e72a15bed6aa6c7666f8668bb5aa9c788f47cada
maaxcode <74791725+maaxcode@users.noreply.github.com>
2023-03-15 16:54:05 +0100

    ajout du .env

--- BotMaison/config.js
+++ BotMaison/config.js
@@ -1,4 +1,5 @@
+
 module.exports = {
 
-    token: "MTA3MjkwMTA0NjMxMTA3NTg4Mg.G95iZc.TaXLUZibb7aau9IamflFLOwIIlwjcMuUEkZn9U"
+    token: process.env.TOKEN
 }
--- maison_bot.js
+++ maison_bot.js
@@ -1,2 +1,4 @@
+require('dotenv').config()
+
 const main= require('./BotMaison/main.js');
 const server= require('./Capteurs/server.js');
